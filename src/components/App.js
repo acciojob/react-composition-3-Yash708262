5f459cce9626bb8995229dd3cac01b057f59a328
Yash708262 <yash708262@gmail.com>
2026-03-25 23:37:53 +0530

    dfs

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tooltip from "./Tooltip";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
+      <div style={{ padding: "50px" }}>
+      <Tooltip text="This is a tooltip">
+        <button>Hover me</button>
+      </Tooltip>
+
+      <br /><br />
+
+      <Tooltip text="Hello from tooltip">
+        <span>Hover over this text</span>
+      </Tooltip>
     </div>
   )
 }
